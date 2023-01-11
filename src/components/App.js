1c9408397ec8bb26c6987f0e43015a9b9291d967
Jose Soto <jsoto7087@gmail.com>
2023-01-10 19:09:26 -0500

    added handle eaten

--- src/components/App.js
+++ src/components/App.js
@@ -5,18 +5,43 @@ import Table from "./Table";
 const API = "http://localhost:3001/sushis";
 
 function App() {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
+  const [wallet, setWallet] = useState(0);
 
   useEffect(()=> {
     fetch(API)
       .then((resp) => resp.json())
-      .then((item)=> setData(item))
+      .then((sushis) => {
+        const updatedShushis= sushis.map((sushi) => {
+          return {...sushi, eaten: false };
+        });
+          setData(updatedShushis)
+      })
   }, [])
+
+  function handleEatSushi(eatenSushi) {
+    if (wallet >= eatenSushi.price) {
+      const updatedShushis = data.map((sushi) => {
+        if (sushi.id === eatenSushi.id) return {...sushi, eaten: true};
+          return data;
+      });
+      setData(updatedShushis);
+      setWallet((wallet) => wallet - eatenSushi.price);
+    } else {
+      alert("Need more money");
+    }
+  }
+
+  function handleAddMoney(moreMoney) {
+    setWallet((wallet) => wallet + moreMoney);
+  }
+
+  const eatenSushis = sushi.filter((sushi) => sushi.eaten); 
 
   return (
     <div className="app">
-      <SushiContainer data={data} />
-      <Table />
+      <SushiContainer data={data}  onEatSushi={handleEatSushi} />
+      <Table wallet={wallet} onAddMoney={handleAddMoney} plates={eatenSushis} />
     </div>
   );
 }
